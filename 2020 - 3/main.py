a429a7c3b6dcd026d68ab1f4feecbdfa7b947a7f
Canopyflick <tenbergeben@gmail.com>
2022-10-09 22:46:39 +0200

    day 3 finished

--- 2020 - 3/main.py
+++ 2020 - 3/main.py
@@ -1,13 +1,6 @@
-def expand_terrain(line):
-    return line * number_of_expansions
-
-
-if __name__ == '__main__':
-
-    file = open("real terrain.txt", "r")
-
-    slope_x = 3
-    slope_y = 1
+def trees_check(slope_x, slope_y):
+    # Again set the pointer to the beginning
+    file.seek(0, 0)
 
     terrain = []
 
@@ -16,55 +9,26 @@ if __name__ == '__main__':
         # remove all newline characters at beginning and end of string
         every_line = every_line.strip()
         terrain.append(every_line)
-    print("Terrain in een list:\n", terrain)
 
     required_width = slope_x * (len(terrain) / slope_y)
-    print("Required width:\n", required_width)
 
     first_row = terrain[0]
     width = len(first_row)
     depth = len(terrain)
 
     number_of_expansions = int(required_width / width) + 1
-    print("Number of expansions:\n", number_of_expansions)
-
-    # 1. Remove newline characters from rows ✔
-    # 2. expand universe with new_terrain ✔
-
-    # Again set the pointer to the beginning
-    # file.seek(0, 0)
 
     # map(function, array)
     # https://www.geeksforgeeks.org/python-map-function/
     terrain = list(map(lambda line_in_terrain: line_in_terrain * number_of_expansions, terrain))
-    print("Terrain =", terrain)
 
-    # universe = []
+    required_width = slope_x * (len(terrain) / slope_y)
 
-    # for every_line in file:
-    #     # remove all newline characters at beginning and end of string
-    #     every_line = every_line.strip()
-    #     universe.append(every_line * number_of_expansions)
-    #
-    # print("\nUniverse in een list:\n", universe)
-
-    # Loop over every row in terrain
-
-    # where's dem trees?
-    # Starting position
     x = 0
     y = 0
-
-    # 0= [0, 0]
-    # 1= [3, 1]
-    # 2= [6, 2]
-    # 3= [9, 3]
-    # 4= [12, 4]
     trees = 0
 
     while y < depth:
-        print("y =\n", y, len(terrain))
-        print("x =\n", x, len(terrain[0]))
         if terrain[y][x] == '#':
             trees = trees + 1
             # trees += 1
@@ -76,15 +40,12 @@ if __name__ == '__main__':
     # for (y = 0; y < depth; y += slope_y) {
     #   for (x = 0; x < width; x += slope_x) {
 
-    print("trees:\n", trees)
+    print("Trees on route for slope Right", slope_x, "Down,", slope_y, ":", trees)
+    return trees
 
-    # for line in terrain:
-    #     # line = terrain[y]
-    #     # character = line[x]
-    #     if terrain[y][x] == '#':
-    #         trees = trees + 1
-    #         # trees += 1
-    #         # trees -= 1
-    #         x += slope_x #
-    #         y += slope_y
-    #     for y in terrain:
+
+if __name__ == '__main__':
+    file = open("real terrain.txt", "r")
+
+    answer = trees_check(1, 1) * trees_check(3, 1) * trees_check(5, 1) * trees_check(7, 1) * trees_check(1, 2)
+    print("All trees multiplied:\n", answer)
